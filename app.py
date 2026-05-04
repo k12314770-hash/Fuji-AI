27e21050db5833a91b1d3ac4d81af2da9d565e23
k12314770-hash <k12314770@gmail.com>
2026-05-04 12:53:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, send_from_directory
 from openai import OpenAI
 import os
 
-app = app = Flask(__name__, static_folder='static', static_url_path='')
+app = Flask(__name__, static_folder='static', static_url_path='')
 
 
 client = OpenAI(
